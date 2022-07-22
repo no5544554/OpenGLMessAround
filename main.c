bb81ceb8c91652414a28b5f374dbe8f5b4fb7a95
no5544554 <lapsley.p@gmail.com>
2022-07-22 09:58:05 -0500

    checkerboard texture test

--- main.c
+++ main.c
@@ -10,6 +10,11 @@
 #define SCREEN_HEIGHT 480
 #define FPS 60
 
+#define CHECKERBOARD_WIDTH  64
+#define CHECKERBOARD_HEIGHT 64
+static GLubyte checkerBoard[CHECKERBOARD_WIDTH * CHECKERBOARD_HEIGHT][4];
+
+
 /* TODO: Turn into a struct */
 float px = 0.0f;
 float pz = 0.0f;
@@ -23,11 +28,31 @@ BOOL leftArrow, rightArrow;
 #define DEG_TO_RAD 0.01745329
 #define NORMALIZED 0.707107
 
+static GLuint texture;
+
 struct
 {
     int start;
     int end;
 } timer;
+
+
+void MakeCheckerboard(void)
+{
+    int x, y, c;
+    for (y = 0; y < CHECKERBOARD_HEIGHT; y++)
+    {
+        for (x = 0; x < CHECKERBOARD_WIDTH; x++)
+        {
+            c = ((((y&0x8)==0)^((x&0x8))==0))*255;
+            checkerBoard[y * CHECKERBOARD_WIDTH + x][0] = (GLubyte) c;
+            checkerBoard[y * CHECKERBOARD_WIDTH + x][1] = (GLubyte) c;
+            checkerBoard[y * CHECKERBOARD_WIDTH + x][2] = (GLubyte) c;
+            checkerBoard[y * CHECKERBOARD_WIDTH + x][3] = (GLubyte) 255;
+        }
+    }
+}
+
 
 void DrawWorld(void)
 {
@@ -40,7 +65,9 @@ void DrawWorld(void)
     glLoadIdentity();
     gluOrtho2D(0, 640, 480, 0);
 
-    // Draw the sky
+    /**
+     * Draw the sky
+     */
     glDisable(GL_LIGHTING);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -109,6 +136,58 @@ void DrawWorld(void)
     glScalef(0.5f, 4.0f, 0.5f);
     glRotatef(90, 1, 0, 0);
     glutSolidCylinder(1.0, 1.0, 16, 1);
+    glPopMatrix();
+
+    // draw checkercube
+    glEnable(GL_TEXTURE_2D);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glPushMatrix();
+    glTranslatef(-5, 2, 0);
+
+    glPushMatrix();
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);   glVertex3f(-1.0, -1.0, 1.0);
+    glTexCoord2f(0.0f, 1.0f);   glVertex3f(1.0, -1.0, 1.0);
+    glTexCoord2f(1.0f, 1.0f);   glVertex3f(1.0, 1.0, 1.0);
+    glTexCoord2f(1.0f, 0.0f);   glVertex3f(-1.0, 1.0, 1.0);
+    glEnd();
+    glPopMatrix();
+
+    glRotatef(90, 0, 1, 0);
+
+    glPushMatrix();
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);   glVertex3f(-1.0, -1.0, 1.0);
+    glTexCoord2f(0.0f, 1.0f);   glVertex3f(1.0, -1.0, 1.0);
+    glTexCoord2f(1.0f, 1.0f);   glVertex3f(1.0, 1.0, 1.0);
+    glTexCoord2f(1.0f, 0.0f);   glVertex3f(-1.0, 1.0, 1.0);
+    glEnd();
+    glPopMatrix();
+
+    glRotatef(90, 0, 1, 0);
+
+    glPushMatrix();
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);   glVertex3f(-1.0, -1.0, 1.0);
+    glTexCoord2f(0.0f, 1.0f);   glVertex3f(1.0, -1.0, 1.0);
+    glTexCoord2f(1.0f, 1.0f);   glVertex3f(1.0, 1.0, 1.0);
+    glTexCoord2f(1.0f, 0.0f);   glVertex3f(-1.0, 1.0, 1.0);
+    glEnd();
+    glPopMatrix();
+
+    glRotatef(90, 0, 1, 0);
+
+    glPushMatrix();
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);   glVertex3f(-1.0, -1.0, 1.0);
+    glTexCoord2f(0.0f, 1.0f);   glVertex3f(1.0, -1.0, 1.0);
+    glTexCoord2f(1.0f, 1.0f);   glVertex3f(1.0, 1.0, 1.0);
+    glTexCoord2f(1.0f, 0.0f);   glVertex3f(-1.0, 1.0, 1.0);
+    glEnd();
+    glPopMatrix();
+
+    glDisable(GL_TEXTURE_2D);
     glPopMatrix();
 
     // Draw "gun"
@@ -248,9 +327,24 @@ int main(int argc, char * argv[])
     glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
     glutCreateWindow("OpenGL");
 
+
+    // Create texture
+    MakeCheckerboard();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, CHECKERBOARD_WIDTH, CHECKERBOARD_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, checkerBoard);
+
+    // set projection
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(60.0, (double)SCREEN_WIDTH / (double)SCREEN_HEIGHT, 0.5, 50.0);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
     glShadeModel(GL_SMOOTH);
 
@@ -266,6 +360,8 @@ int main(int argc, char * argv[])
 
     glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
 
     glutDisplayFunc(Display);
     glutTimerFunc(1000 / FPS, RunMainLoop, 0);
